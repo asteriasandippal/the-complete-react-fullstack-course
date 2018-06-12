b430db590a9fdcc64728bc5913e8048b7cf7bb7a
asteriasandippal <sandip.pal@asteria.co.in>
2018-06-12 17:07:20 +0530

    Adds returning Arrays

--- reactlifecycles/src/containers/Posts.jsx
+++ reactlifecycles/src/containers/Posts.jsx
@@ -3,13 +3,27 @@ import { Link } from 'react-router-dom';
 
 class Posts extends Component {
   render() {
-
+   const ids = [
+      {id: 1, name: 'Post 1'},
+      {id: 2, name: 'Post 2'},
+      {id: 3, name: 'Post 3'},
+      {id: 4, name: 'Post 4'}
+    ];
+  
+   const list = ids.map(item => {
+      return (
+        <span key={item.id}>
+          <Link to={`${this.props.match.url}/${item.id}`}>{item.name}</Link>
+        </span>
+      )
+    });
     return (
       <div className="Posts">
-        <Link to={`${this.props.match.url}/1`}>Post 1</Link>
+        {/* <Link to={`${this.props.match.url}/1`}>Post 1</Link>
         <Link to={`${this.props.match.url}/2`}>Post 2</Link>
         <Link to={`${this.props.match.url}/3`}>Post 3</Link>
-        <Link to={`${this.props.match.url}/4`}>Post 4</Link>
+        <Link to={`${this.props.match.url}/4`}>Post 4</Link> */}
+        {list}
       </div>
     );
   }
