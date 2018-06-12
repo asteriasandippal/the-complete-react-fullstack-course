840337c488d3702e09e87c2dddd852fbda634339
asteriasandippal <sandip.pal@asteria.co.in>
2018-06-12 16:14:57 +0530

    Adds React lifeScycles Continued

--- reactlifecycles/src/containers/LifeCycles.jsx
+++ reactlifecycles/src/containers/LifeCycles.jsx
@@ -9,11 +9,44 @@ class Lifecycles extends Component {
     componentWillMount() {
         console.log('Before Render');
     }
+
+    // Before Update
+    componentWillUpdate() {
+        console.log('BEFORE UPDATE');
+    }
+    // Before Update
+    componentDidUpdate() {
+        console.log('AFTER UPDATE');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log(nextState.title);
+        console.log(this.state.title);
+        if(nextState.title === 'Change LifeCycles More') {
+            return true
+        }
+        return false;
+    }
+
+    componentWillReceiveProps() {
+        console.log('BEFORE RECEIVE PROPS');
+    }
+
+    componentWillUnmount() {
+        console.log('UNMOUNT');
+    }
+    
     //  Render
     render() {
+        console.log('RENDER');
         return (
         <div className="Lifecycles">
             <h3>{this.state.title}</h3>
+            <div onClick={
+                () => this.setState(() => ({
+                    title: 'Change LifeCycles More'
+                }))
+            }>CLICK TO CHANGE</div>
         </div>
         );
     }
