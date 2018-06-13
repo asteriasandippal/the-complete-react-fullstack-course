89a32d1e7b7939e4f238d86fcd99aefb30312db4
asteriasandippal <sandip.pal@asteria.co.in>
2018-06-13 14:11:46 +0530

    Apply CSS transition more

--- transitions/src/components/CSStransition.js
+++ transitions/src/components/CSStransition.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import '../css/App.css';
 
 class Fade extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        }
+        this.showDiv = this.showDiv.bind(this);
+    }
+    showDiv() {
+        this.setState(() => ({
+            show: !this.state.show ? true : false
+        }));
+    }
+
     render(){
         return(
             <div>
-                CSS TRANSITIONS
+                <CSSTransition
+                    in={this.state.show}
+                    timeout={2000}
+                    classNames="square"
+                >
+                    <div className={`square ${this.state.show}`}>
+                        CSS TRANSITIONS
+                    </div>
+                </CSSTransition>
+                <div className="showDiv" onClick={this.showDiv}>
+                    Show or Hide
+                </div>
             </div>
         )
     }
