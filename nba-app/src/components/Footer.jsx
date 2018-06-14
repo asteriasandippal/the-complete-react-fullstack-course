a835dd6e99d8494617fe8f63b18cc09e5f13bb5c
asteriasandippal <sandip.pal@asteria.co.in>
2018-06-14 19:59:00 +0530

    Adds DB json & modifies Footer component

--- nba-app/src/components/Footer.jsx
+++ nba-app/src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Logo from './Logo';
+
+import {CURRENT_YEAR} from '../constants/appConstants';
+import Nbalogo from '../assets/images/nba_logo.png';
 
 function Footer() {
     return (
-        <footer>
-            Footer
+        <footer className="footer">
+            <Logo
+                link="/"
+                src={Nbalogo}
+                title="NBA Aplication"
+                className="footer__logo"
+            />
+            <div className="footer__right">
+                &copy; {CURRENT_YEAR} All rights reserved.
+            </div>
         </footer>
     );
 }
