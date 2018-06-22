82c22be265f2b7475beb8f422f5861d7286164b0
asteriasandippal <sandip.pal@asteria.co.in>
2018-06-22 15:29:14 +0530

    Implements NewsLists

--- nba-app/src/containers/Home/NewsLists.jsx
+++ nba-app/src/containers/Home/NewsLists.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+// import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import { API_URL } from '../../constants/appConstants'; 
@@ -13,21 +13,57 @@ class NewsLists extends React.Component {
             end: this.props.start + this.props.amount,
             amount: this.props.amount,
         }
+        this.renderNews = this.renderNews.bind(this);
+        this.requestData = this.requestData.bind(this);
+        this.loadMore = this.loadMore.bind(this);
     }
 
     componentWillMount() {
-        axios.get(`${API_URL}/articles?_start=${this.state.start}&_end=${this.state.end}`)
-            .then(response => {
-                this.setState(() => ({
-                    items: [...this.state.items, ...response.data]
-                }));
-            });
+        this.requestData(this.state.start, this.state.end);
+    }
+
+    requestData(start, end) {
+        axios.get(`${API_URL}/articles?_start=${start}&_end=${end}`)
+        .then(response => {
+            this.setState(() => ({
+                items: [...this.state.items, ...response.data]
+            }));
+        });
+    }
+
+    renderNews(type) {
+        let template = null;
+        switch(type) {
+            case('card'):
+                template = this.state.items.map((item, i) => (
+                    <div className="newsList__item" key={i}>
+                        <div className="newsList__itemBlock">
+                            <Link to={`/articles/${item.id}`}>
+                                <h2>{item.title}</h2>
+                            </Link>
+                        </div>
+                    </div>
+                ));
+                break;
+            default:
+                template = null;
+        }
+
+        return template;
+    }
+
+    loadMore() {
+        const ended = this.state.end + this.state.amount;
+        this.requestData(this.state.end, ended);
     }
 
     render() {
         return (
-            <div>
-                sas
+            <div className="newsList">
+                {this.renderNews(this.props.type)}
+                <div onClick={this.loadMore}>
+                    LOAD MORE
+                </div>
             </div>
         );
     }
