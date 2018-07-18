c24f5f49d9be8734a94ddca9c99fe280b34f0403
asteriasandippal <sandip.pal@asteria.co.in>
2018-07-18 19:32:58 +0530

    Adds Video List  - adds video container list
    Signed-off-by: asteriasandippal <sandip.pal@asteria.co.in>

--- nba-app/src/containers/PrivateRouter.jsx
+++ nba-app/src/containers/PrivateRouter.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Layout from './Layout';
 import Articles from './../containers/Articles';
+import VideosSingle from './../containers/Videos/VideosSingle';
 import Videos from './../containers/Videos';
 import News from './../containers/News';
 
@@ -14,7 +15,8 @@ function PrivateRouter() {
                 <Route path="/" exact component={Home}/>
                 <Route path="/news" component={News}/>
                 <Route path="/articles/:id" component={Articles}/>
-                <Route path="/videos/:id" component={Videos}/>
+                <Route path="/videos/:id" component={VideosSingle}/>
+                <Route path="/videos" component={Videos}/>
             </Switch>
         </Layout>
     );
