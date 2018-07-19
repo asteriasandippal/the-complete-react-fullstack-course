0092a4e9e2f218489d23610ea72fa783187bec44
asteriasandippal <sandip.pal@asteria.co.in>
2018-07-19 12:46:52 +0530

    Setups Work form  - adds controlled form
    Signed-off-by: asteriasandippal <sandip.pal@asteria.co.in>

---
+++ work-form/src/components/controlled.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class Controlled extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'Jhone',
+            lastName: 'Mickel' 
+        }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleLastNameChange = this.handleLastNameChange.bind(this);
+    }
+
+    handleNameChange(event) {
+        this.setState({
+            name: event.target.value
+        });
+    }
+    handleLastNameChange(event) {
+        this.setState({
+            lastName: event.target.value
+        });
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <form>
+                    <div className="form_element">
+                        <label>Enter Name</label>
+                        <input 
+                            type="text"
+                            value={this.state.name}
+                            onChange = {this.handleNameChange}
+                        />
+                    </div>
+                    <div className="form_element">
+                        <label>Enter Last Name</label>
+                        <input 
+                            type="text"
+                            value={this.state.lastName}
+                            onChange = {this.handleLastNameChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Controlled;
