5f7b55eff3f4dbe92b1aaa7d85650eb8fbdeb5ad
asteriasandippal <sandip.pal@asteria.co.in>
2018-07-23 18:14:37 +0530

    Adds Authentication
    Signed-off-by: asteriasandippal <sandip.pal@asteria.co.in>

--- firebase-form/src/firebase.js
+++ firebase-form/src/firebase.js
@@ -12,34 +12,10 @@ const config = {
 firebase.initializeApp(config);
 
 const firebaseDB = firebase.database();
+const googleAuth = new firebase.auth.GoogleAuthProvider();
 
 export {
-    firebaseDB
+    firebase,
+    firebaseDB,
+    googleAuth
 }
-// firebaseDB.ref('user').push({
-//     name: 'Akash',
-//     lastName: 'Kumar',
-//     age: 31,
-//     car : {
-//         brand: 'Volvo',
-//         color: 'Black'
-//     }
-// });
-
-// firebaseDB.ref('user').once('value')
-//     .then((snapshot) => {
-//         console.log(snapshot.val());
-//         const users = [];
-//         snapshot.forEach((childSnapshot) => {
-//             users.push({
-//                 id: childSnapshot.key,
-//                 ...childSnapshot.val()
-//             });
-//         });
-//         console.log(users);
-//     });
-
-// firebaseDB.ref('user').orderByChild('lastName').equalTo('Kumar').once('value')
-//     .then((snapshot) => {
-//         console.log(snapshot.val());
-//     })
